dc581801be167721aa66f747129bf4c1deff7855
John <john.hoffman@uon.edu.au>
2021-03-19 12:03:47 +1100

    Initial commit

---
+++ OpenTK Tutorial in WPF/ExampleScene.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Wpf;
+
+namespace OpenTK_Tutorial_in_WPF
+{
+    // Not sure if GLWpfController handles this, but we are missing swapbuffers, onresize and onunload
+    // Don't need double buffers - I think this is auto done through directX interop
+    class ExampleScene
+    {
+        private int VertexBufferObject;
+        private Shader shader;
+        private int VertexArrayObject;
+        private bool prepared = false;
+        // Rectangle
+        private int ElementBufferObject;
+        private float[] RectangleVertices = {
+             0.5f,  0.5f, 0.0f,  // top right
+             0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
+            };
+
+        private uint[] RectangleIndices = {  // note that we start from 0!
+                0, 1, 3,   // first triangle
+                1, 2, 3    // second triangle
+            };
+        public void Prepare()
+        {
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, RectangleVertices.Length * sizeof(float), RectangleVertices, BufferUsageHint.StaticDraw);
+
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            // It's almost exactly the same as how you use the VBO! Most of OpenGL's buffer types will follow this pattern:
+            // Create with GL.GenBuffer(), bind with GL.BindBuffer, and then use GL.BufferData to add data to it.
+            // We create/bind the Element Buffer Object EBO the same way as the VBO, except there is a major difference here which can be REALLY confusing.
+            // The binding spot for ElementArrayBuffer is not actually a global binding spot like ArrayBuffer is. 
+            // Instead it's actually a property of the currently bound VertexArrayObject, and binding an EBO with no VAO is undefined behaviour.
+            // This also means that if you bind another VAO, the current ElementArrayBuffer is going to change with it.
+            // Another sneaky part is that you don't need to unbind the buffer in ElementArrayBuffer as unbinding the VAO is going to do this,
+            // and unbinding the EBO will remove it from the VAO instead of unbinding it like you would for VBOs or VAOs.
+            ElementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            // We also upload data to the EBO the same way as we did with VBOs.
+            GL.BufferData(BufferTarget.ElementArrayBuffer, RectangleIndices.Length * sizeof(uint), RectangleIndices, BufferUsageHint.StaticDraw);
+            // The EBO has now been properly setup. Go to the Render function to see how we draw our rectangle now!
+
+
+            shader = new Shader("../../../Shaders/shader.vert", "../../../Shaders/shader.frag");
+            prepared = true;
+        }
+        public void PrepareTriangle()
+        {
+            VertexBufferObject = GL.GenBuffer();
+            VertexArrayObject = GL.GenVertexArray();
+            // This locks any buffer calls we make to the currently bound buffer
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            // Triangle of vertices
+            float[] vertices = {
+                -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+                 0.5f, -0.5f, 0.0f, //Bottom-right vertex
+                 0.0f,  0.5f, 0.0f  //Top vertex
+            };
+
+            shader = new Shader("../../../Shaders/shader.vert", "../../../Shaders/shader.frag");
+
+            /*
+            // Add the vertices to the buffer's memory
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float),
+                vertices, BufferUsageHint.StaticDraw);
+            // StaticDraw: the data will most likely not change at all or very rarely.
+            // DynamicDraw: the data is likely to change a lot.
+            // StreamDraw: the data will change every time it is drawn.
+
+            // Tell openGL how to interpret the vertex data
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            */
+
+
+            // Handling a VAO
+            // ..:: Initialization code (done once (unless your object frequently changes)) ::..
+            // 1. bind Vertex Array Object
+            GL.BindVertexArray(VertexArrayObject);
+            // 2. copy our vertices array in a buffer for OpenGL to use
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            // 3. then set our vertex attributes pointers
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            prepared = true;
+        }
+
+        public void Render()
+        {
+            if (!prepared)
+            {
+                return;
+            }
+            GL.ClearColor(Color4.Red); // Can set this in the prepare rather than each render call
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            //GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            GL.DrawElements(PrimitiveType.Triangles, RectangleIndices.Length, DrawElementsType.UnsignedInt, 0);
+        }
+
+        public void Close()
+        {
+            // Binding a buffer to 0 basically sets it to null, so any calls that modify a buffer without binding one first will result in a crash. This is easier to debug than accidentally modifying a buffer that we didn't want modified.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            // Delete all buffer objects
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteBuffer(VertexArrayObject);
+            shader.Dispose();
+        }
+    }
+}
---
+++ OpenTK Tutorial in WPF/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenTK.Wpf;
+
+
+namespace OpenTK_Tutorial_in_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ExampleScene exampleScene;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var settings = new GLWpfControlSettings
+            {
+                MajorVersion = 4,
+                MinorVersion = 3
+            };
+            OpenTkControl.Start(settings);
+            exampleScene = new ExampleScene();
+        }
+
+        private void OpenTkControl_OnRender(TimeSpan delta)
+        {
+            double width = OpenTkControl.ActualWidth;
+            double height = OpenTkControl.ActualHeight;
+            Point p = Mouse.GetPosition(OpenTkControl);
+            if (p.X <= width && p.Y <= height && p.X >= 0 && p.Y >= 0)
+            {
+                Trace.WriteLine(p);
+            }
+            exampleScene.Render();
+        }
+
+        private void OpenTkControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            exampleScene.Prepare();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            exampleScene.Close();
+        }
+    }
+}
